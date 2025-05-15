98288c319f91bf40cf935103bf9ae015b5b99b12
KatsaWins <chr.katsaros99@gmail.com>
2025-05-15 12:27:47 +0300

    Fix DB_NAME to match Render PostgreSQL database

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const port = process.env.PORT || 5000;
 const db = new pg.Client({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: "inotes",
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
