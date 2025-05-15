d5feb663903c3029fe2b88277ddd10976c0e39f5
ChrKatsaros <chr.katsaros99@gmail.com>
2025-05-15 20:33:02 +0300

    Update Notes.jsx

--- frontend/src/components/Notes.jsx
+++ frontend/src/components/Notes.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 // Ανάλογα με το περιβάλλον, χρησιμοποιεί κατάλληλο URL
-const API_URL = "http://localhost:5000";
+const API_URL = "https://inoteapp-backend.onrender.com";
 
 function Notes() {
   const [note, setNote] = useState({
